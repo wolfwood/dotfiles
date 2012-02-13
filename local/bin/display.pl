ed2c043f44a3aacbe9173319147fa011c252f1af
wolfwood <wolfwood@xomb.org>
2012-02-13 13:47:19 -0800

    after changing parameters, prompt for 'can you see this?' reset display on any key other than yes

--- local/bin/display.pl
+++ local/bin/display.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/perl -w
 
+# needed to read a key without hitting enter
+use Term::ReadKey;
+ReadMode 4; # Turn off controls keys
+
+
 use strict;
 my($arg);
 
@@ -22,16 +27,37 @@ foreach $arg (@ARGV){
 my($lcd, $external) = ("LVDS1", "VGA1");
 
 
-if($reset == 1){
-		`xrandr --output $external --off`;
-		`xrandr --output $lcd --auto`;
-}elsif($nomirror == 1){
-		`xrandr --output $lcd --off`;
-		`xrandr --output $external --auto`;
-}elsif($present == 1){
-		# XXX: find largest common mode between displays
-		my($mode) = "1024x768";
+my($tryagain, $key) = 1;
 
-		`xrandr --output $lcd --mode $mode`;
-		`xrandr --output $external --mode $mode`;
+while($tryagain){
+		if($reset == 1){
+				`xrandr --output $external --off`;
+				`xrandr --output $lcd --auto`;
+		}elsif($nomirror == 1){
+				`xrandr --output $lcd --off`;
+				`xrandr --output $external --auto`;
+		}elsif($present == 1){
+				# XXX: find largest common mode between displays
+				my($mode) = "1024x768";
+
+				`xrandr --output $lcd --mode $mode`;
+				`xrandr --output $external --mode $mode`;
+		}
+
+
+		print "Is it safe? ";
+		while (not defined ($key = ReadKey(-1))) {# No key yet
+		}
+
+		print "$key\n";
+
+		if($key eq 'y'){
+				$tryagain = 0;
+		}else{
+				$reset = 1;
+				$nomirror = 0;
+				$present = 0;
+		}
 }
+
+ReadMode 0; # Reset tty mode before exiting
